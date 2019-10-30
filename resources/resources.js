d60d89c7c63910b241f6f3efb9cebc55bba87b5e
Sam McBroom <sammcb314@gmail.com>
2019-10-30 09:59:40 -0400

    Chaned eating guide link to published page.

--- resources/resources.js
+++ resources/resources.js
@@ -1,6 +1,6 @@
 var resourceNames = ["Veggies of Virginia FAQ", "Eating in Charlottesville", "Vegan Job Guide", "A Week of Meals"];
 
-var resourceInfos = ["Check out the guide <a href=\"assets/files/vov_faq.pdf\">here</a>!", "Check out the guide <a href=\"https://docs.google.com/document/d/1xlkeFdMjyR1tgeJz35Owo3LdTzYtgEww5gu8uPVN16g/edit?usp=sharing\">here</a>!", "Check out the guide <a href=\"assets/files/vegan_jobs.pdf\">here</a>!", "Check out the menu <a href=\"assets/files/menu_0.pdf\">here</a>!"];
+var resourceInfos = ["Check out the guide <a href=\"assets/files/vov_faq.pdf\">here</a>!", "Check out the guide <a href=\"https://docs.google.com/document/d/e/2PACX-1vR1tNpBtMmY-q1GHaxUhbJPd649K8YXdzIHmAYa1-IoPvifdaPd_OZ6kyMa5jPXJ996lgqH7WC-t0T8/pub\">here</a>!", "Check out the guide <a href=\"assets/files/vegan_jobs.pdf\">here</a>!", "Check out the menu <a href=\"assets/files/menu_0.pdf\">here</a>!"];
 
 var resourceDates = ["Monday, Nov. 12, 2018", "Monday, Nov. 12, 2018", "Monday, Nov. 12, 2018", "Monday, Nov. 12, 2018"];
 
