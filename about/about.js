e409937d0cbca2dfbbef4916dc4b0ddaed7e7ef1
Sam McBroom <sammcb314@gmail.com>
2019-12-24 02:15:03 -0500

    Updated exec. Fixed bug with image centering.

--- about/about.js
+++ about/about.js
@@ -1,12 +1,67 @@
-var execNames = ["Camille Rovani", "Courtney Jacobs", "Sam McBroom", "Josh Crane", "Alicia Zheng", "Madison Crouch", "Brooke Crouch", "Haley Dues"];
+var execNames = [
+  "Sam McBroom",
+  "Brooke Crouch",
+  "Michael Nicklas",
+  "Riley Phillips",
+  "Courtney Jacobs",
+  "Madison Crouch",
+  "Natalie Finman",
+  "Haley Dues",
+  "Alicia Zheng",
+  "Chelsea Ulrich"
+];
 
-var execPositions = ["President", "Vice President", "Webmaster, Treasurer", "Secretary", "Advocacy Leader", "Advocacy Leader", "Social Chair", "Outreach Chair"];
+var execPositions = [
+  "President, Webmaster",
+  "Vice President",
+  "Vice President",
+  "Secretary",
+  "Treasurer",
+  "Social Chair",
+  "Social Chair",
+  "Outreach Chair",
+  "Advocacy Chair",
+  "Advocacy Chair"
+];
 
-var execYears = ["Fourth Year", "Third Year", "Third Year", "Third Year", "Third Year", "Second Year", "Second Year", "Second Year"];
+var execYears = [
+  "Third Year",
+  "Second Year",
+  "Third Year",
+  "Second Year",
+  "Third Year",
+  "Second Year",
+  "Third Year",
+  "Second Year",
+  "Third Year",
+  "Second Year"
+];
 
-var execMajors = ["Majors: Art History, French", "Major: Computer Science", "Majors: Computer Science, Physics", "Major: Interdisciplinary Studies", "Global Sustainability, Spanish", "Majors: Civil Engineering, Global Sustainability", "Majors: Global Sustainability, Environmental Science", "Majors: Civil Engineering, Architecture, Business"];
+var execMajors = [
+  "Majors: Computer Science, Physics",
+  "Majors: Global Sustainability, Environmental Science",
+  "Major: Unknown",
+  "Major: Unknown",
+  "Major: Computer Science",
+  "Majors: Civil Engineering, Architecture, Business",
+  "Major: Unknown",
+  "Majors: Civil Engineering, Global Sustainability",
+  "Majors: Global Sustainability, Spanish",
+  "Major: Unknown"
+];
 
-var execPhotos = ["camille_rovani.png", "courtney_jacobs.png", "sam_mcbroom.png", "josh_crane.png", "alicia_zheng.png", "madison_crouch.png", "brooke_crouch.png", "haley_dues.png"];
+var execPhotos = [
+  "sam_mcbroom",
+  "brooke_crouch",
+  "michael_nicklas",
+  "riley_phillips",
+  "courtney_jacobs",
+  "madison_crouch",
+  "natalie_finman",
+  "haley_dues",
+  "alicia_zheng",
+  "chelsea_ulrich"
+];
 
 function addExec() {
   var photoFits = {};
@@ -20,10 +75,10 @@ function addExec() {
       }
       if (Object.keys(photoFits).length === execNames.length) {
         for (i = 0; i < execNames.length; i++) {
-          document.getElementById("exec").innerHTML += '<div class="exec-member"><div class="exec-photo"><div class="exec-photo-container"><img class=' + photoFits[execPhotos[i]] + ' src="assets/images/' + execPhotos[i] + '"/></div></div><div class="exec-info"><h4>' + execNames[i] + '</h4><p>' + execPositions[i] + '</p><p>' + execYears[i] + '</p><p>' + execMajors[i] + '</p></div></div>';
+          document.getElementById("exec").innerHTML += '<div class="exec-member"><div class="exec-photo"><div class="exec-photo-container"><img class=' + photoFits[execPhotos[i]] + ' src="assets/images/' + execPhotos[i] + '.png"/></div></div><div class="exec-info"><h4>' + execNames[i] + '</h4><p>' + execPositions[i] + '</p><p>' + execYears[i] + '</p><p>' + execMajors[i] + '</p></div></div>';
         }
       }
     });
-    photo.src = "assets/images/" + name;
+    photo.src = "assets/images/" + name + ".png";
   });
 }
